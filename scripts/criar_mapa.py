fe6462eb7f25960079996a6a41efc1346e8c7775
iuryxavierdp <iuryxavierdp@gmail.com>
2025-11-19 17:13:33 +0000

    Changes to be committed: 	modified:   scripts/criar_mapa.py
    Changes not staged for commit:
    	modified:   mapa.html
    	deleted:    scripts/criar-mapa-EN.py

--- scripts/criar_mapa.py
+++ scripts/criar_mapa.py
@@ -3,8 +3,9 @@ import folium as fl
 from branca.element import IFrame
 
 #dir = 'C:/Users/Iury/Desktop/IF/ProjetoMapa'
-# dir = 'F:\IF\ProjetoMapa'
-dir = 'C:/Users/Iury/Desktop/Nova pasta'
+#dir = 'F:\IF\ProjetoMapa'
+#dir = 'C:/Users/Iury/Desktop/Nova pasta'
+dir = '/workspaces/IFEmpresas'
 df_empresas = pd.read_excel(f'{dir}/dados/empresa.xlsx', sheet_name=0)
 df_empresas_en = pd.read_excel(f'{dir}/dados/empresa_en.xlsx', sheet_name=0)
 
